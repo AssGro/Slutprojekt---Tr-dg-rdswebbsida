4f47786f5fb90ad1819c2327d5be0a4447391763
AssGro <94969384+AssGro@users.noreply.github.com>
2022-04-15 16:34:24 +0200

    Delete js directory

--- js/somthin.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-let menuElement = document.querySelector(".hamburge-menu")
-
-let button = menuElement.querySelector("button")
-
-button.addEventListener("click", () => {
-    menuElement.classList.toggle("open")
-})
